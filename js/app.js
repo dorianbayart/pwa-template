36a2e5e54db728e4fb8a0ad7e49093718d70a76b
Dorian Bayart <dorianbayart@users.noreply.github.com>
2020-02-07 15:48:07 +0100

    Testing Camera on iOS PWA (with FileInput)

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function () {
+window.addEventListener('load', function () {
+	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.register('service-worker.js')
 			.then(function (reg) {
-				handleStateChange();
 				console.log('[ServiceWorker] Registered', reg);
 			}).catch(function (error) {
 				console.error('[ServiceWorker] Not registered', error);
 			});
-	});
-}
+	
+	}
+	handleStateChange();
+});
 
 window.addEventListener('online', handleStateChange);
 window.addEventListener('offline', handleStateChange);
 
-var connexionStatusTarget = document.getElementById('connexion_status');
-
 function handleStateChange() {
-  var materialIcon = navigator.onLine ? 'signal_cellular_4_bar' : 'signal_cellular_connected_no_internet_4_bar';
-  var label = navigator.onLine ? 'Online' : 'Offline';
-  var color = navigator.onLine ? 'color-green' : 'color-red';
-  connexionStatusTarget.innerHTML = '<i class="material-icons ' + color + '">' + materialIcon + '</i><span class="label">' + label + '</span>';
+	var connexionStatusTarget = document.getElementById('connexion_status');
+	var materialIcon = navigator.onLine ? 'signal_cellular_4_bar' : 'signal_cellular_connected_no_internet_4_bar';
+	var label = navigator.onLine ? 'Online' : 'Offline';
+	var color = navigator.onLine ? 'color-green' : 'color-red';
+	connexionStatusTarget.innerHTML = '<i class="material-icons ' + color + '">' + materialIcon + '</i><span class="label">' + label + '</span>';
 }
